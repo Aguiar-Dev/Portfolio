1e440a2b6f74999316bdd2d0fe372bf71d1f657d
FernandoAguiarGuevarez <flaguiarguevarez@fullsail.edu>
2017-03-02 13:22:26 -0500

    Deleted unnecessary lines of code which were being overwritten almost immediately. Modified the structure of the code again to include functions outside of each other and not all inside one another. OpOrganized the variable declarations to be easily recognizable.

--- Javascript_Duel/js/main.js
+++ Javascript_Duel/js/main.js
@@ -26,17 +26,14 @@ var players = [{
     }
 ];
 
-/**** Round VAR Declaration ****/
+/**** Variable Declarations ****/
 var round = 0;
-
-/**** Display Round ****/
 var display = document.getElementById('roundCount');
-display.defaultValue = players[0].Name + ' VS ' + players[1].Name;
-
-/**** Show Player name and Health ****/
 var p1Health = document.getElementById('player1P');
-p1Health.innerHTML = players[0].Name + ' HP: ' + players[0].Hp;
 var p2Health = document.getElementById('player2P');
+
+/**** Modify Player names and health displays ****/
+p1Health.innerHTML = players[0].Name + ' HP: ' + players[0].Hp;
 p2Health.innerHTML = players[1].Name + ' HP: ' + players[1].Hp;
 display.defaultValue = 'Round: ' + round + ' - BEGIN';
 
@@ -45,64 +42,64 @@ function gameStart() {
     /**** Code for Fight Button ****/
     button.innerHTML = 'FIGHT!';
 
-    /**** Declaring Functions ****/
-    var fight = function() {
-
-        var minDmg = {
-            player1: players[0].Dmg * 0.5,
-            player2: players[1].Dmg * 0.5
-        };
-
-        var dmg = {
-            player1: Math.ceil(Math.random() * (players[0].Dmg - minDmg.player1) + minDmg.player1),
-            player2: Math.ceil(Math.random() * (players[1].Dmg - minDmg.player2) + minDmg.player2)
-        };
-
-        //Subtracting Player Damage from HP
-        players[0].Hp -= dmg.player1;
-        players[1].Hp -= dmg.player2;
-
-        //Call the WinnerCheck function and store the returned data
-        var results = winnerCheck();
-
-        //Check results for any outcome and break out of the for loop
-        if (results === 'No Winner') {
-            round++;
-            p1Health.innerHTML = players[0].Name + ' HP: ' + players[0].Hp;
-            p2Health.innerHTML = players[1].Name + ' HP: ' + players[1].Hp;
-            display.defaultValue = 'Round: ' + round + ' - BEGIN';
-        } else {
-            p1Health.innerHTML = players[0].Name + ' HP: ' + players[0].Hp;
-            p2Health.innerHTML = players[1].Name + ' HP: ' + players[1].Hp;
-            display.defaultValue = results;
-            display.style.fontSize = 'x-small';
-            button.innerHTML = 'Restart';
-            button.removeEventListener('click', gameStart, false);
-            button.addEventListener('click', restart, false);
-        }
-
-    };
-
-    var winnerCheck = function() {
-        //Set results to no winner
-        var result = 'No Winner';
-
-        //Change results depending on the outcome using conditionals
-        if (players[0].Hp < 1 && players[1].Hp < 1) {
-            result = 'You both died. DRAW';
-        } else if (players[0].Hp < 1) {
-            result = players[1].Name + ' Wins!';
-        } else if (players[1].Hp < 1) {
-            result = players[0].Name + ' Wins!';
-        }
-
-        //Return the result into the fight function
-        return result;
-    };
-
     /**** Main Code Starts Here ****/
     fight();
 }
+
+/**** Declaring Functions ****/
+var fight = function() {
+
+    var minDmg = {
+        player1: players[0].Dmg * 0.5,
+        player2: players[1].Dmg * 0.5
+    };
+
+    var dmg = {
+        player1: Math.ceil(Math.random() * (players[0].Dmg - minDmg.player1) + minDmg.player1),
+        player2: Math.ceil(Math.random() * (players[1].Dmg - minDmg.player2) + minDmg.player2)
+    };
+
+    //Subtracting Player Damage from HP
+    players[0].Hp -= dmg.player1;
+    players[1].Hp -= dmg.player2;
+
+    //Call the WinnerCheck function and store the returned data
+    var results = winnerCheck();
+
+    //Check results for any outcome and break out of the for loop
+    if (results === 'No Winner') {
+        round++;
+        p1Health.innerHTML = players[0].Name + ' HP: ' + players[0].Hp;
+        p2Health.innerHTML = players[1].Name + ' HP: ' + players[1].Hp;
+        display.defaultValue = 'Round: ' + round + ' - BEGIN';
+    } else {
+        p1Health.innerHTML = players[0].Name + ' HP: ' + players[0].Hp;
+        p2Health.innerHTML = players[1].Name + ' HP: ' + players[1].Hp;
+        display.defaultValue = results;
+        display.style.fontSize = 'x-small';
+        button.innerHTML = 'Restart';
+        button.removeEventListener('click', gameStart, false);
+        button.addEventListener('click', restart, false);
+    }
+
+};
+
+var winnerCheck = function() {
+    //Set results to no winner
+    var result = 'No Winner';
+
+    //Change results depending on the outcome using conditionals
+    if (players[0].Hp < 1 && players[1].Hp < 1) {
+        result = 'You both died. DRAW';
+    } else if (players[0].Hp < 1) {
+        result = players[1].Name + ' Wins!';
+    } else if (players[1].Hp < 1) {
+        result = players[0].Name + ' Wins!';
+    }
+
+    //Return the result into the fight function
+    return result;
+};
 
 var restart = function() {
     /**** Player One VARS ****/
